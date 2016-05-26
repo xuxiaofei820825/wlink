8d87ffd5d53dd15b509d2a8fc4dcd1a4d842630b
xuxiaofei820825 <flying.xu77@gmail.com>
2016-05-26 17:32:29 +0800

    Add files via upload

---
+++ wlink-core-api/src/main/java/com/iauto/wlink/core/api/App.java
@@ -0,0 +1,13 @@
+package com.iauto.wlink.core.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
